fab084614cc50b02c0ed95be06a06557205c19ec
kadhar17 <kadhar17@gmail.com>
2021-12-16 15:39:01 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/HelloWorldWebApplication1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldWebApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldWebApplication1Application.class, args);
+	}
+
+}
