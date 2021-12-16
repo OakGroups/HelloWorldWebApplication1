9098c7884d6d51b6fac37aa41b20d0122b85e2c9
kadhar17 <kadhar17@gmail.com>
2021-12-16 15:49:10 -0500

    New change

--- src/main/java/com/example/demo/HelloWorldWebApplication1Application.java
+++ src/main/java/com/example/demo/HelloWorldWebApplication1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldWebApplication1Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HelloWorldWebApplication1Application.class, args);
 	}
 
