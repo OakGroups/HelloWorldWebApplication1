27349765bad6785df8dddaefff58e46171136a97
kadhar17 <kadhar17@gmail.com>
2021-12-16 21:20:17 -0500

    latest change in pom.xml

--- src/main/java/com/example/demo/HelloWorldWebApplication1Application.java
+++ src/main/java/com/example/demo/HelloWorldWebApplication1Application.java
@@ -2,9 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
-public class HelloWorldWebApplication1Application {
+public class HelloWorldWebApplication1Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 
